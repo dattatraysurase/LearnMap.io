4478fbbcb067a2e0a29d008e86b5718d5750aab6
dattatraysurase <dattatraysurase59@gmail.com>
2025-11-08 20:16:44 +0530

    setup session & cookies

--- Server/index.js
+++ Server/index.js
@@ -1,14 +1,32 @@
 import express from "express"
 import { config } from "dotenv"
+import session from "express-session";
+import cookieParser from "cookie-parser";
+import cors from "cors";
 config();
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 // middlewares
+app.use(cors({
+     origin:"*",
+     credentials:true,
+}))
  app.use(express.json());
  app.use(express.urlencoded({extended:true}));
+ app.use(cookieParser());
 
+
+ app.use(session({
+    secret:process.env.SESSION_SECREAT,
+    resave:false,
+    saveUninitialized:false,
+    cookie:{
+        httpOnly:true,
+        maxAge: 1000 * 60 * 60 * 24 * 7,// 7 days
+    }
+ }))
 
 
 
